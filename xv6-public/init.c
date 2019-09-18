0c901da35ebe8573bf61f3d2359b8ddaf84b38aa
Jorge Quintero <vinicioquinterobuss@gmail.com>
2019-09-18 01:27:58 +0000

    Practice 2. Creating a custom welcome message and avoid causing a Kernel panic

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+    printf(1, "Welcome to Ventanas 9\nBy Vini\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
